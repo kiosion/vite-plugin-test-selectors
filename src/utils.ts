f1b8f3faaa98082939eb3293e6da337110fe394a
kiosion <hi@kio.dev>
2022-07-28 02:01:39 -0300

    Update util regex

--- src/utils.ts
+++ src/utils.ts
@@ -5,14 +5,15 @@ const getRegexps = (selectors: string[]) => {
     arr.push(
       new RegExp(`(?:\\s)+${selector}-(?:.{1,}?[\\s=])(?:(?:["'].*?["'])*|.*?(?=[\\s/>]))`, 'm')
     );
-
     // attr_dev(element, 'data-test-id', 'something');
     arr.push(
       new RegExp(
-        `(?:^|.*)["']${selector}-(?:.{1,}?)["']+(?:(?:,(?: *["']+.*?["']+)|(?:.*?[);]$))*(?:.*?,){0,1}(?:\\s{0,1})|.*?[);]$)`,
+        `^.*attr_dev.*?["']${selector}-(?:.+?)["'],(?:\\s)*(?:(?:["'].*\\w["'])|(?:.*\\w)).*$(?:\\n{0,1})`,
         'm'
       )
     );
+    // ...{ props: 'something', 'data-test-id', 'something' | boolean }...
+    arr.push(new RegExp(`,{0,1}\\s*?["']${selector}-(?:.+?)["'](?::\\s{0,1}(?:.*\\w))`, 'm'));
   });
 
   return arr;
